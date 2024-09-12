6c6da0708cdbe7b294e221fa737ace63c75c9cd8
Sachin Sharma <sach2996@gmail.com>
2024-09-11 23:51:21 -0400

    project added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,42 @@
+import Project from "./Project";
+import "./Projects.css";
 export default function Projects() {
-  return <div>Projects Page</div>;
+  const project = {
+    name: "Netflix Clone",
+    tech: "React, Recoil",
+  };
+  return (
+    <>
+      <div className="projects-container">
+        <div
+          className="project-details"
+          style={{
+            backgroundImage: "url(public/netflix.jpg)",
+          }}
+        >
+          <div style={{ borderRadius: "5px" }}>
+            <a href="https://your-netflix-clone-url.com">
+              <img
+                src="public/netflix-favicon.png"
+                width={"100px"}
+                height={"100px"}
+              />
+            </a>
+          </div>
+          {/* <div>
+            <h3>{project.name}</h3>
+          </div> */}
+          <div className="netflix-clone-link">
+            <a href="https://your-netflix-clone-url.com">Netflix Clone</a>
+          </div>
+        </div>
+        {/* <Project /> */}
+        <Project />
+      </div>
+      <div className="projects-container">
+        <Project />
+        <Project />
+      </div>
+    </>
+  );
 }
